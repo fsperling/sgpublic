61e614c978ad5050923a1c3f4d39a6530d175aad
Felix Sperling <f.sperling@gmx.de>
2015-12-02 17:36:07 +0800

    refactor route map controller

--- app/controllers/route_map_controller.rb
+++ app/controllers/route_map_controller.rb
@@ -3,62 +3,37 @@ require 'json'
 require 'pry'
 
 class RouteMapController < ApplicationController
+
   def show
     @features = []
     @lines_json = []
     lines = Busline.limit(50).where("direction == '1'").all.to_a
-
-    lines.each do |line| 
-      line_coords = []
-      add_features_for_stop(line, line.busstops.first.busstop_detail)
-      add_features_for_stop(line, line.busstops.last.busstop_detail)
-      line.busstops.map(&:busstop_detail).each do |stop|
-
-        unless stop == nil
-        if stop.long == nil || stop.lat == nil
-          #log error: no coords
-        else
-          #@features += [{type: 'Feature', properties: {name: stop.road}, geometry: {type: 'Point', coordinates: [stop.long, stop.lat]} }]
-          line_coords += [[stop.long, stop.lat]]
-        end
-        end
-      end
-      color = "#" +  "%06x" % (rand * 0xffffff)
-      @lines_json += [{type: 'LineString', coordinates: line_coords, style: { color: color, opacity: 0.5, weight: 2 }}] 
-
-      #binding.pry
-    end
+    generate_json_for(lines)
   end
 
   def night
     @features = []
     @lines_json = []
     lines = Busline.where("freq_am_peak == '-' and freq_am_off == '-' and freq_pm_peak == '-' and freq_pm_off != '-'").all.to_a
-
-    lines.each do |line| 
-      line_coords = []
-      add_features_for_stop(line, line.busstops.first.busstop_detail)
-      add_features_for_stop(line, line.busstops.last.busstop_detail)
-
-      line.busstops.map(&:busstop_detail).each do |stop|
-        unless stop == nil
-          if stop.long == nil || stop.lat == nil
-            #log error: no coords
-          else
-            #@features += [{type: 'Feature', properties: {name: stop.road}, geometry: {type: 'Point', coordinates: [stop.long, stop.lat]} }]
-            line_coords += [[stop.long, stop.lat]]
-          end
-        end
-      end
-      color = "#" +  "%06x" % (rand * 0xffffff)
-      @lines_json += [{type: 'LineString', coordinates: line_coords, style: { color: color, opacity: 0.5, weight: 2 }}] 
-
-    end
+    generate_json_for(lines)
   end
 
   def add_features_for_stop(line, stop)
       desc = "<b>" + line.busnumber.to_s + "</b>: " + stop.road + ", " + stop.desc + " (" + stop.busstop_id.to_s + ")"
       @features += [{type: 'Feature', properties: {name: stop.road, popupContent: desc}, geometry: {type: 'Point', coordinates: [stop.long, stop.lat]} }]
 
+  end
+
+  def generate_json_for(lines)
+    lines.each do |line| 
+      add_features_for_stop(line, line.busstops.first.busstop_detail)
+      add_features_for_stop(line, line.busstops.last.busstop_detail)
+
+      line_coords = line.get_coords
+      color = "#" +  "%06x" % (rand * 0xffffff)
+      @lines_json += [{type: 'LineString', coordinates: line_coords, style: { color: color, opacity: 0.5, weight: 2 }}] 
+
+      #binding.pry
+    end
   end
 end
--- app/models/busline.rb
+++ app/models/busline.rb
@@ -1,7 +1,26 @@
 class Busline < ActiveRecord::Base
-      has_many :busstops, foreign_key: 'busnumber', primary_key: 'busnumber'
+  has_many :busstops, foreign_key: 'busnumber', primary_key: 'busnumber'
 
-      validates :busnumber, presence: true
-      validates :start_code, presence: true
-      validates :end_code, presence: true
+  validates :busnumber, presence: true
+  validates :start_code, presence: true
+  validates :end_code, presence: true
+
+  def get_coords
+    line_coords = []
+    self.busstops.map(&:busstop_detail).each do |stop|
+
+      unless stop == nil
+        if stop.long == nil || stop.lat == nil
+          #log error: no coords
+        else
+          line_coords += [[stop.long, stop.lat]]
+        end
+      end
+    end
+    line_coords
+  end
+
 end
+
+
+#@features += [{type: 'Feature', properties: {name: stop.road}, geometry: {type: 'Point', coordinates: [stop.long, stop.lat]} }]

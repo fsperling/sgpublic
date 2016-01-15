b02e20e7c93d05445e2bf611f4c6966f0f44f1fd
Felix Sperling <f.sperling@gmx.de>
2016-01-15 11:19:51 +0800

    try to get the rest imported with heroku

--- app/workers/lta_api_worker.rb
+++ app/workers/lta_api_worker.rb
@@ -6,10 +6,10 @@ class LtaApiWorker
   sidekiq_options retry: false
 
   def perform
-    LtaApi.new.get_data_for('SBSTInfo')
-    LtaApi.new.get_data_for('SMRTInfo')
-    LtaApi.new.get_data_for('SBSTRoute')
-    LtaApi.new.get_data_for('SMRTRoute')
+#    LtaApi.new.get_data_for('SBSTInfo')
+#    LtaApi.new.get_data_for('SMRTInfo')
+#    LtaApi.new.get_data_for('SBSTRoute')
+#    LtaApi.new.get_data_for('SMRTRoute')
     LtaApi.new.get_data_for('BusStopCode')
 
     LtaData.new.check_data
